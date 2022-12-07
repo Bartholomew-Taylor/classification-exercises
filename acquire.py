f52a107f61fa7db651ee7cef58db1c5159bc0f85
Bart Taylor <bartholomew.s.taylor@gmail.com>
2022-12-07 16:57:47 -0600

    end of day

--- acquire.py
+++ acquire.py
@@ -30,7 +30,7 @@ def get_iris_data():
         return pd.read_csv(filename)
     else:
         # read the SQL query into a dataframe
-        df = pd.read_sql('SELECT * FROM measurements JOIN species USING(spcies_id)', get_connection('iris_db'))
+        df = pd.read_sql('SELECT * FROM measurements JOIN species USING(species_id)', get_connection('iris_db'))
 
         # Write that dataframe to disk for later. Called "caching" the data for later.
         df.to_csv(filename)
@@ -40,13 +40,13 @@ def get_iris_data():
 
 
 def get_telco_data():
-    filename = "iris.csv"
+    filename = "telco_churn.csv"
 
     if os.path.isfile(filename):
         return pd.read_csv(filename)
     else:
         # read the SQL query into a dataframe
-        df = pd.read_sql('SELECT * FROM customers JOIN species USING(spcies_id)', get_connection('telco_churn'))
+        df = pd.read_sql('SELECT * FROM customers JOIN contract_types USING(contract_type_id) JOIN internet_service_types USING(internet_service_type_id) JOIN payment_types USING(payment_type_id)', get_connection('telco_churn'))
 
         # Write that dataframe to disk for later. Called "caching" the data for later.
         df.to_csv(filename)
