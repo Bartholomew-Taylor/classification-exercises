ebeb3d0aa57ee7ace8ee2241f9de17e53aa12e43
Bart Taylor <bartholomew.s.taylor@gmail.com>
2022-12-07 16:06:31 -0600

    near end of day

---
+++ acquire.py
@@ -0,0 +1,56 @@
+import os
+import pandas as pd
+from scipy import stats
+from pydataset import data
+import numpy as np
+import env
+
+def get_connection(db, username=env.username, host=env.host, password=env.password):
+    return f'mysql+pymysql://{username}:{password}@{host}/{db}'
+
+def get_titanic_data():
+    filename = "titanic.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+
+def get_iris_data():
+    filename = "iris.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('SELECT * FROM measurements JOIN species USING(spcies_id)', get_connection('iris_db'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+
+
+def get_telco_data():
+    filename = "iris.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        # read the SQL query into a dataframe
+        df = pd.read_sql('SELECT * FROM customers JOIN species USING(spcies_id)', get_connection('telco_churn'))
+
+        # Write that dataframe to disk for later. Called "caching" the data for later.
+        df.to_csv(filename)
+
+        # Return the dataframe to the calling code
+        return df  
+    
